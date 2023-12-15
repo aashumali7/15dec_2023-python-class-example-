cf1b4a88c00747a538799ec4b136ceb3fbcf63dc
aashumali <aashishmali223@gmail.com>
2023-12-15 10:18:33 +0530

    new

---
+++ aashish.py
@@ -0,0 +1,20 @@
+class A():
+    #1 property
+    name =''
+    surname = ''
+    #2 constructor
+    def __init__(self,x ,y):
+        self.name = x
+        self.surname = y
+    #3 method
+    def myName(self):
+        print(f'my name is {self.name}')
+        print(f'my surname is {self.surname}')
+
+#create class object 
+#classobject = ClassName
+co = A('aashish','mali')
+#classobject.method
+co.myName()
+
+
